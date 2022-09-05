4819a4a0daa4762c8b243e2e351fbee908f71a1a
ManzDev <JoseRomanHernandez@gmail.com>
2022-09-05 01:19:31 +0100

    Update elements and checktests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./components/CardElement.js";
-import { elements } from "./modules/readFiles.js";
+import { elements, allElements as allImageElements } from "./modules/readFiles.js";
+import resultElements from "./assets/elements.json";
 
 const container = document.querySelector(".container");
 
@@ -8,3 +9,61 @@ elements.forEach(name => {
   element.setAttribute("type", name);
   container.appendChild(element);
 });
+
+// Test
+
+const resultCrafteableElements = Object.keys(resultElements).sort();
+const allCrafteableElements = [...new Set(Object.entries(resultElements).flat(Infinity))];
+// console.log(allImageElements);
+
+const checkAllElements = () => {
+  allCrafteableElements.forEach(element => {
+    const existImage = allImageElements.includes(element);
+    const imagesCheck = existImage ? "✅" : "❌";
+    !existImage && console.log(`${element} in JSON ✅, in Images ${imagesCheck}`);
+  });
+};
+
+const checkAllCombinations = (currentElements) => {
+  const iterableElements = Object.entries(resultElements);
+  let i = 0;
+
+  while (i < iterableElements.length) {
+    const [resultItem, combinations] = iterableElements[i];
+    combinations.forEach(([firstElement, secondElement]) => {
+      const isCrafteable =
+        !currentElements.includes(resultItem) &&
+        currentElements.includes(firstElement) &&
+        currentElements.includes(secondElement);
+
+      if (isCrafteable) {
+        i = 0;
+        currentElements.push(resultItem);
+      }
+    });
+    i++;
+  }
+
+  console.log(currentElements);
+};
+
+window.findCombination = (elements) => {
+  const [firstElement, secondElement] = elements.sort();
+
+  const iterableElements = Object.entries(resultElements);
+
+  const result = iterableElements.find(([resultElement, combinations]) => {
+    return combinations.find(([a, b]) => a === firstElement && b === secondElement);
+  });
+
+  if (result) {
+    console.log("Elemento generado: " + result[0]);
+    result[1].forEach(([a, b]) => console.log(`Combinaciones posibles: ${a} + ${b}`));
+  } else {
+    console.log("No hay combinaciones.");
+  }
+};
+
+const initialElements = structuredClone(elements);
+checkAllElements();
+checkAllCombinations(initialElements);
--- src/modules/readFiles.js
+++ src/modules/readFiles.js
@@ -1,13 +1,19 @@
-// const GLOB = import.meta.glob("../../public/icons/*.svg");
-const GLOB = {
+const ALL_ELEMENTS = import.meta.glob("../../public/icons/*.svg");
+const INITIAL_ELEMENTS = {
   "../../public/icons/html.svg": null,
   "../../public/icons/css.svg": null,
   "../../public/icons/javascript.svg": null,
-  "../../public/icons/web-components.svg": null
+  "../../public/icons/web-components.svg": null,
+  "../../public/icons/terminal.svg": null
 };
 
-const FILES = Object.keys(GLOB);
-export const elements = FILES.map(element => element
-  .replace("../../public/icons/", "")
-  .replace(".svg", ""));
-// console.log(elements);
+const filterName = (elements) => {
+  const FILES = Object.keys(elements);
+  return FILES
+    .map(element => element
+      .replace("../../public/icons/", "")
+      .replace(".svg", ""));
+};
+
+export const elements = filterName(INITIAL_ELEMENTS);
+export const allElements = filterName(ALL_ELEMENTS);
