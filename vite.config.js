b5a2aa8cdd338e2714a18dea5c3fe0a58a55bd70
ManzDev <JoseRomanHernandez@gmail.com>
2023-10-19 22:43:25 +0100

    Update config and packages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import path from "path";
 const isGitHubPages = true;
-const folderName = path.basename(process.cwd()) + "/";
+const folderName = `${path.basename(process.cwd())}/`;
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
-const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
+const base = mode === "production" && isGitHubPages ? `/${folderName}` : "/";
 
 export default {
   root: "src",
