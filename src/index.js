8e47aabd15cdbc271fa8210b351346641062e8b4
ManzDev <JoseRomanHernandez@gmail.com>
2026-08-02 18:35:38 +0100

    Minor SVG changes and responsive changes

--- src/index.js
+++ src/index.js
@@ -1,45 +1,18 @@
 import { INITIAL_ELEMENTS, checkAllCombinations } from "./modules/game.js";
-import elements from "./assets/elements.js";
 import "./components/CardElement.js";
 import "./components/ScoreBoard.js";
-
-// eslint-disable-next-line
-const icons = import.meta.glob("../public/icons/*.svg");
-
-const removePathAndExtension = (paths) =>
-  Object.keys(paths).map((path) =>
-    path.replace("../public/icons/", "").replace(".svg", ""));
-
-const initialElements = INITIAL_ELEMENTS;
-const allElementsFromImages = removePathAndExtension(icons);
 
 const container = document.querySelector(".container");
 const scoreBoard = document.querySelector("score-board");
 
 // Elementos iniciales de partida
-initialElements.forEach((name) => {
+INITIAL_ELEMENTS.forEach((name) => {
   const element = document.createElement("card-element");
   element.setAttribute("type", name);
   container.appendChild(element);
 });
 
-const crafteable = checkAllCombinations(initialElements);
-const notCrafteable = allElementsFromImages.filter((key) => !crafteable.includes(key));
+const crafteable = checkAllCombinations(INITIAL_ELEMENTS);
 
-scoreBoard.setElements(initialElements.length);
+scoreBoard.setElements(INITIAL_ELEMENTS.length);
 scoreBoard.setTotal(crafteable.length);
-
-// Diagnóstico en consola
-console.log("Crafteable:", crafteable.sort().join(", "));
-console.log("No crafteable:", notCrafteable.sort().join(", "));
-
-// Elementos del JSON que no tienen imagen en public/icons
-const checkAllElements = () => {
-  Object.keys(elements).forEach((element) => {
-    if (!allElementsFromImages.includes(element)) {
-      console.log(`${element} in JSON ✅, in Images ❌`);
-    }
-  });
-};
-
-checkAllElements();
